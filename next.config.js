3d6c7b46f9147a78918b021d4d883e15a7316e79
Matt Hamlin <matthewjameshamlin@gmail.com>
2021-08-14 14:04:26 -0400

    Updates

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 const withMDX = require('@next/mdx')()
+
 module.exports = withMDX({
   pageExtensions: ['mdx', 'jsx', 'js', 'ts', 'tsx'],
   experimental: {
     reactRefresh: true,
     modern: true,
-    reactMode: 'concurrent',
+    reactRoot: 'concurrent',
   },
 })
